755380a7e09e65f0c7c2ba2491c68ad866c860a2
mireu-lab <limmireu@kakao.com>
2022-04-22 19:18:22 +0000

    Reset stopwatch, add comments and comment for follow-up support

--- App/docker_build/golang.py
+++ App/docker_build/golang.py
@@ -8,20 +8,23 @@ client = docker.from_env()
 def build(filename):
     data = json.load(open("Data/set.json", 'r'))
 
-    build_start = time()
-    docker_contalner = str(client.containers.create(data['golang']).id)
+    build_start = time() # 컨테이너 빌드 시간
+    docker_contalner = str(client.containers.create(data['golang']).id) # 컨테이너 종류 / 컨테이너 실행
 
-    os.system(f"docker cp Data/user_file/{filename} {docker_contalner}:home/run.cpp")
+    os.system(f"docker cp Data/user_file/{filename} {docker_contalner}:home/run.cpp") # 컨테이너에 파일 전송
+    build_end = time() # 컨테이너 빌드 종료 시간
 
     data = client.containers.get(docker_contalner)
-    data.restart()
-    build_end = time()
+    data.restart() # 컨테이너 재시작
 
-    sleep(1)
+    sleep(1) # 1초 딜레이
 
-    run_start = time()
-    return_data = data.logs()
-    data.remove()
-    run_end = time()
+    run_start = time() # 프로그램 실행 시간
     
-    return {"test build language" : "golang", "container build time" : build_end - build_start, "file execution time" : run_end - run_start, "file result" : return_data}
+    return_data = data.logs() # 프로그램 실행
+    
+    run_end = time() # 프로그램 종료 시간
+    
+    data.remove() # 컨테이너 삭제
+    
+    return {"test build language" : "python", "container build time" : build_end - build_start, "file execution time" : run_end - run_start, "file result" : return_data}
